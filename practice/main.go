03b894f074339dfcd3460d84ab1ae5a49ffa36f0
Axalate <ridmatharaka99@gmail.com>
2022-05-25 00:58:04 +0530

    changes

--- practice/main.go
+++ practice/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
@@ -9,5 +11,12 @@ func main() {
 	var aString string = "This is Go!"
 	fmt.Println(aString)
 	fmt.Printf("The variable's type is %T\n", aString)
+
+	reader := bufio.NewReader(os.Stdin);
+
+	fmt.Println("Enter text: ");
+	input,_ := reader.ReadString('\n')
+
+	fmt.Println(input);
 
 }
